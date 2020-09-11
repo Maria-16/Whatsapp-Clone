f49cf193e974066058285dc7ea3b180d3ad446bb
Maria-16 <mariaali2106@gmail.com>
2020-09-11 19:33:16 +0500

    Version 1

---
+++ app/src/main/java/com/example/whatsapp/Home_screen.java
@@ -0,0 +1,22 @@
+package com.example.whatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class Home_screen extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private Toolbar toolbar;
+    viewPagerAdapter viewPagerAdapter;
+
+
+    }
+}
